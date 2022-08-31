89fa35fb71ea5265453e82fddf2909193a7ef853
rhyun9584 <rhyun9584@naver.com>
2022-08-31 17:03:52 +0900

    2186번 / 코드개선

--- python/2186.py
+++ python/2186.py
@@ -8,11 +8,9 @@ dp = [[[0] * (len_word+1) for _ in range(M)] for _ in range(N)]
 dx = [1, -1, 0, 0]
 dy = [0, 0, 1, -1]
 def dfs(x, y, cnt):
-    if cnt == len_word and board[x][y] == word[-1]:
+    if cnt == len_word:
         dp[x][y][-1] = 1
         return 1
-    if dp[x][y][cnt] > 0:
-        return dp[x][y][cnt]
 
     for i in range(4):
         for j in range(1, K+1):
