58ff8c56c15c0b78446b597c6a6bfe6c92eeae47
rhyun9584 <rhyun9584@naver.com>
2022-09-20 17:37:20 +0900

    11657 / 코드 개선\n벨만포드 알고리즘을 SPFA으로 변경

--- python/11657.py
+++ python/11657.py
@@ -2,24 +2,37 @@ from collections import deque
 
 N, M = map(int, input().split())
 
-edges = []
+edges = [[] for _ in range(N+1)]
 for _ in range(M):
-    edges.append(tuple(map(int, input().split())))
+    a, b, c = map(int, input().split())
+    edges[a].append((b, c))
 
 dist = [int(1e9)] * (N+1)
+is_inQ = [0] * (N+1)
+cnt = [0] * (N+1)
+
+queue = deque()
+queue.append(1)
 dist[1] = 0
+is_inQ[1] = 1
 
 cycle = False
-for i in range(N):
-    for s, e, c in edges:
-        if dist[s] != int(1e9) and dist[s] + c < dist[e]:
-            if i == N-1:
-                cycle = True
-                break
-            dist[e] = dist[s] + c
-    
-    if cycle: break
+while queue:
+    v = queue.popleft()
+    is_inQ[v] = 0
+    cnt[v] += 1
 
+    if cnt[v] == N:
+        cycle = True
+        break
+
+    for e, c in edges[v]:
+        if dist[v] + c < dist[e]:
+            dist[e] = dist[v] + c
+            if is_inQ[e] == 0:
+                queue.append(e)
+                is_inQ[e] = 1
+    
 if cycle:
     print(-1)
 else:
