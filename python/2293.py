09c7b3bd2ecbd22438a4b2b71574c55388dfd31d
rhyun9584 <rhyun9584@naver.com>
2022-09-26 20:47:23 +0900

    2293번 / 코드 개선

--- python/2293.py
+++ python/2293.py
@@ -2,16 +2,11 @@ import sys
 
 N, K = map(int, input().split())
 coins = list(map(int, sys.stdin.readlines()))
-coins.sort()
 
-dp = [[1] + [0] * K for _ in range(2)]
-for i in range(1, N+1):
-    c = coins[i-1]
-    now = i % 2
-    before = abs(now-1)
-    for j in range(1, K+1):
-        dp[now][j] = dp[before][j]
-        if c <= j:
-            dp[now][j] += dp[now][j-c]
+dp = [0] * (K+1)
+dp[0] = 1
+for c in coins:
+    for i in range(c, K+1):
+        dp[i] += dp[i-c]
 
-print(dp[N%2][K])
+print(dp[K])
